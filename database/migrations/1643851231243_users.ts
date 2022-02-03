d6f4ad1661d7e46ff2c1bd649da89d4a9dcc75ac
black0utt <wesleysv61@gmail.com>
2022-02-02 22:54:25 -0300

    first commit

---
+++ database/migrations/1643851231243_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unique();
+      table.string('name', 50).notNullable();
+      table.integer('idade', 5).notNullable();
+      table.string('email').unique();
+
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
